d1468db3347b9ab0256942ad61629e64db7b6d67
Jaime Alves Marinho de Oliveira <ixquitilis@Jaimes-MacBook-Air.local>
2018-11-08 22:26:36 -0300

    ready for a chal

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class ServerElementComponent implements OnInit {
-  @Input('srvElement') element: {type: string, name: string, content: string};
+  // tslint:disable-next-line:no-input-rename
+  @Input('srvElement') element: {
+    type: string,
+    name: string,
+    content: string
+  };
 
-  constructor() { }
+  constructor() {
+
+   }
 
   ngOnInit() {
   }
